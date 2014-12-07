20cce0238ab7ebf090495a1d000b681d3d608a55
Hans Schollaardt <hansschollaardt@hotmail.com>
2014-12-07 19:20:31 +0100

    voorbereiding opdracht

--- Opdracht2/src/RSHeap.java
+++ Opdracht2/src/RSHeap.java
@@ -1,30 +1,44 @@
-
+import java.util.Scanner;
 
 
 public class RSHeap {
-private static final int INITIAL_HEAP_SIZE = 5;
-private static final int RANDOM_NUMBERS = 20000;
-private static final int RANDOMIZE_AROUND = 50000;
-private int[] random;
+	private static final int HEAP_SIZE;
+	private static final int RANDOM_NUMBERS = 2000;
+	private static final int RANDOMIZE_AROUND = 5000;
+	private int[] random, heap;
+	private boolean done;
+	private Scanner in;
 
-public RSHeap() {
-	init();
-	//generate random numbers
-	
-	//feed numbers to IN
-//		while(in.hasNext()){
-//			
-//		}
-	
-}
+	public RSHeap(int heapsize) {
+		init(heapsize);
+		done = false;
+		//while file still holds more values, continue
+		while(in.hasNext()){
+			buildHeap();
+			while();
+			
+			done = true;
+			
+			//write out to file
+		}
+		
+	}
 
-private void init() {
-	random = new int[RANDOM_NUMBERS];
-	for (int i = 0; i < random.length; i++) {
-		random[i] = (int) (Math.random() * RANDOMIZE_AROUND);
-		System.out.println(random[i]);
+	private void init(int heapsize) {
+		HEAP_SIZE = heapsize;
+		in = new Scanner("inputfile.txt");
+		heap = new int[HEAP_SIZE];
+		random = new int[RANDOM_NUMBERS];
+		// generate random numbers to fill array
+		for (int i = 0; i < random.length; i++) {
+			random[i] = (int) (Math.random() * RANDOMIZE_AROUND);
+			// System.out.println(random[i]);
+		}
+
 	}
 	
-}
+	private void buildHeap(){
+		
+	}
 
 }
