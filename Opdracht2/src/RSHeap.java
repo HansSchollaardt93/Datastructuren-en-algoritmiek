efe2fddb98067eb693b773198e42aa195599373b
HansSchollaardt93 <hansschollaardt@hotmail.com>
2014-12-10 13:49:16 +0100

    shitloads

--- Opdracht2/src/RSHeap.java
+++ Opdracht2/src/RSHeap.java
@@ -8,57 +8,69 @@ import java.util.Scanner;
  */
 public class RSHeap {
 	private static int HEAP_SIZE;
+	private static int DEADSPACE_SIZE;
 	private static final int RANDOM_NUMBERS = 200;
 	private static final int RANDOMIZE_AROUND = 500;
-	
+	private int indexRandomArray;
 	private int[] random, heap;
-	
-	private boolean done;
+
 	private Scanner in;
 	private PrintWriter out;
 
 	public RSHeap(int heapsize) {
 		init(heapsize);
 
-		int smallest;
-		int next;
-//		done = false;
-		// read HEAP_SIZE-elements into heap-array
 
-		buildHeap(heap);
+		int next;
+		// read HEAP_SIZE-elements into heap-array
+		
+		buildHeap();
 		// while array still holds more values, continue
-		while (in.hasNext()) {
+		while (hasNext()) {
 			// remove smallest element from heap and write this to OUT
-			smallest = getSmallest();
-			writeToOutput(smallest); 
+			int smallest = findMin();
+			
 			next = Integer.parseInt(in.nextLine());
+			printToOutput(smallest); 
 			
-			
+			heap[0] = heap[HEAP_SIZE];
+			HEAP_SIZE--;
+			perculateDown(0, HEAP_SIZE);
 			if (next >= smallest) { // next fits into current run
 			// insert next into heap
+				HEAP_SIZE++;
+				heap[HEAP_SIZE] = next;
+				perculateUp(0, HEAP_SIZE);	//re-order the heap
 			} else {
 				// Heapsize H--, Deadspace++
-				// insert next into heap
+				perculateDown(0, HEAP_SIZE);
+				HEAP_SIZE--;
+				DEADSPACE_SIZE++;
+				// insert next into deadspace
 			}
 			if (heap.length == 0){
-				buildheap(dead space);
+				buildHeap();
 			}
-//			done = true;
 
-			// write out to file
+			// write single value out to file
 		}
-		//write rest of heap to OUT
+		//write remainder of heap to OUT
 		printToOutput(heap);
-		buildheap(dead space);
-//		write rest to OUT
+		buildHeap();
+//		write remainder to OUT
 		printToOutput(rest);
 		in.close();
 		out.close();
 
 	}
 
+	private boolean hasNext() {
+		return (indexRandomArray >= 0);
+	}
+
 	/**
 	 * Method to print the remainder of arrays to output file
+	 * 
 	 * @param array
 	 *            array to write to file
 	 */
@@ -68,21 +80,26 @@ public class RSHeap {
 
 	/**
 	 * Method to print single integer to output file
+	 * 
 	 * @param smallest
 	 */
-	private void writeToOutput(int smallest) {
+	private void printToOutput(int smallest) {
 		out.println(smallest);
 	}
 
 	/**
 	 * Initial method to setup constants and initialize values
+	 * 
 	 * @param heapsize
 	 *            Desired heapsize (length of array)
 	 */
 	private void init(int heapsize) {
 		HEAP_SIZE = heapsize;
+		DEADSPACE_SIZE = 0;
+
 		heap = new int[HEAP_SIZE];
 		random = new int[RANDOM_NUMBERS];
+		indexRandomArray = random.length - 1;
 		// generate random numbers to fill array
 		for (int i = 0; i < random.length; i++) {
 			random[i] = (int) (Math.random() * RANDOMIZE_AROUND);
@@ -90,7 +107,7 @@ public class RSHeap {
 		}
 		// Initialize in- and outputs
 		try {
-			in = new Scanner("inputfile.txt");
+			// in = new Scanner("inputfile.txt");
 			out = new PrintWriter("outputfile.txt");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -98,26 +115,129 @@ public class RSHeap {
 	}
 
 	/**
-	 * Method to (re-)build the heap-array, according to the specification of
-	 * binary heap.
-	 * @param array
-	 *            Array to rebuild in good heap format
+	 * Method to get the smallest value off of the heap
+	 * 
+	 * @return smallest value in array
 	 */
-	private void buildHeap(int[] array) {
+	private int findMin() {
+		return heap[0];
+	}
 
+/**
+ * 
+ * @param startIndex
+ */
+	private void perculateDown(int startIndex, int lastIndex) {
+		int left = 0, right = 0, parent = 0;
+		parent = heap[startIndex];
+
+		// check if children are smaller then your own value
+		if (hasLeftChild(startIndex)) {
+			/* PSEUDOCODE
+			 * if has right
+			 *  compare left and right
+			 *  if (left < right) 
+			 *  	swap left with parent
+			 *  	perculateDown(index left)
+			 *  else 
+			 *  	swap right with parent
+			 *  	perculateDown(index right)
+			 * else if left < parent
+			 *   swap with parent
+			 *   perculateDown(index left)
+			 */
+			int tempvalue;
+			left = getLeftChild(startIndex);
+			if(hasRightChild(startIndex)){
+				right = getRightChild(startIndex);
+				if(left<right){
+					//swap left with parent
+					tempvalue = left;
+					parent = left;
+					parent = tempvalue;
+					//continue perculate at left childs position
+					perculateDown(getLeftChild(startIndex));
+					//right is bigger than left
+				} else {
+					//swap right with parent
+					tempvalue = right;
+					parent = right;
+					parent = tempvalue;
+					//continue perculate at right childs position
+					perculateDown(getRightChild(startIndex));
+				}
+			//Only has left child
+			} else {
+				if(left < parent){
+					//swap left with parent
+					tempvalue = left;
+					parent = left;
+					parent = tempvalue;
+					//continue perculate at left childs position
+					perculateDown(getLeftChild(startIndex));
+				}
+				//do nothing, numbers are in right order
+			}
+		}
 	}
 
 	/**
-	 * Method to get the smallest value off of the heap
-	 * @return smallest value in array
+	 * Method to (re-)build the heap-array, according to the specification of
+	 * binary heap.
+	 * 
+	 * @param array
+	 *            Array to rebuild in good heap format
 	 */
-	private int getSmallest() {
-		int smallest = 0;
-		for (int i = 0; i < heap.length; i++) {
-			if (heap[i] <= smallest) {
-				smallest = heap[i];
-			}
+	private void buildHeap() {
+
+		// perculate down
+		for (int i = ((heap.length / 2) - 1); i >= 0; i--) {
+			perculateDown(i);
 		}
-		return smallest;
+	}
+
+	private void deleteMin() {
+		// delete index 0
+		heap[0] = heap[HEAP_SIZE];
+		// perculate down
+		perculateDown(0);
+	}
+
+	private void insert(int toInsert) {
+
+	}
+
+	private boolean isEmpty() {
+		return HEAP_SIZE > 0;
+	}
+
+	private int getLeftChild(int index) {
+		if (!isLeaf(index)) {
+			return ((2 * index));
+		}
+		return -1;
+	}
+
+	private int getRightChild(int index) {
+		if (!isLeaf(index)) {
+			return ((2 * index) + 1);
+		}
+		return -1;
+	}
+
+	private int getParent(int index) {
+		return (index / 2);
+	}
+
+	private boolean isLeaf(int index) {
+		return ((index >= heap.length / 2) && (index < heap.length));
+	}
+
+	private boolean hasLeftChild(int index) {
+		return (getLeftChild(index) != -1);
+	}
+
+	private boolean hasRightChild(int index) {
+		return (getRightChild(index) != -1);
 	}
 }
