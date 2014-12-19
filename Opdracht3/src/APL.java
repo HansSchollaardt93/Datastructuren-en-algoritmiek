6b001fd35b55a52c0fae5705e3edc1e70f4513df
BenjaminFaal <benjamin_faal@hotmail.com>
2014-12-19 15:31:13 +0100

    added inner-class Node to the RedTrie

--- Opdracht3/src/APL.java
+++ Opdracht3/src/APL.java
@@ -3,8 +3,15 @@ import java.io.IOException;
 public class APL {
 
 	public static void main(String[] args) throws IOException {
-		RedTrie<String> trie = new RedTrie<String>();
-
+		RedTrie<Integer> trie = new RedTrie<Integer>();
+		trie.insert("ball", 1);
+		trie.insert("bat", 2);
+		trie.insert("doll", 3);
+		trie.insert("dork", 4);
+		trie.insert("do", 5);
+		trie.insert("dorm", 6);
+		trie.insert("send", 7);
+		trie.insert("sense", 8);
 	}
 
 }
--- Opdracht3/src/RedTrie.java
+++ Opdracht3/src/RedTrie.java
@@ -7,13 +7,14 @@
  *            the Type of the data to store
  */
 public class RedTrie<T> {
+
 	/**
 	 * Method to insert data with a given key ?
 	 * 
 	 * @param s
 	 *            the key of the data?
 	 * @param data
-	 *            the data itself
+	 *            the 'index' of the String s
 	 */
 	public void insert(String s, T data) {
 
@@ -40,4 +41,76 @@ public class RedTrie<T> {
 	 */
 	public void delete(String s) {
 	}
+
+	/**
+	 * Inner-class which will hold the data and the child of a Node of the
+	 * RedTrie.
+	 * 
+	 * @author Benjamin & Hans Schollaardt.
+	 * 
+	 */
+	private class Node {
+		private T data;
+		private Node child;
+
+		/**
+		 * Method to get the 'identifier' of this Node.
+		 * 
+		 * @return the 'identifier' of this Node
+		 */
+		public T getData() {
+			return data;
+		}
+
+		/**
+		 * Method to set the 'identifier' of this Node.
+		 */
+		public void setData(T data) {
+			this.data = data;
+		}
+
+		/**
+		 * Method to get the 'next' Node.
+		 * 
+		 * @return the 'next' Node of this Node.
+		 */
+		public Node getChild() {
+			return child;
+		}
+
+		/**
+		 * Method to set the 'next' Node of this Node.
+		 * 
+		 * @return the 'next' of the
+		 */
+		public void setChild(Node child) {
+			this.child = child;
+		}
+
+		/**
+		 * 
+		 * @return whether this Node has a child Node.
+		 */
+		public boolean hasChild() {
+			return child != null;
+		}
+
+		/**
+		 * Removes the child Node of this Node by setting it to null.
+		 */
+		public void removeChild() {
+			this.child = null;
+		}
+
+		/**
+		 * A Node is a leave when it has not further children.
+		 * 
+		 * @return whether this Node is a leave or not.
+		 */
+		public boolean isLeave() {
+			return child == null;
+		}
+
+	}
+
 }
