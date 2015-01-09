2637f0593ecb9cb121f6dbc16f91a57cd3fb339e
BenjaminFaal <benjamin_faal@hotmail.com>
2015-01-09 16:23:38 +0100

    unit tests added

--- Opdracht 3 Generic/src/APL.java
+++ Opdracht 3 Generic/src/APL.java
@@ -1,5 +1,6 @@
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -9,18 +10,26 @@ import java.util.Scanner;
  */
 public class APL {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
+		long start = System.currentTimeMillis();
+
 		Trie<Data> trie = new Trie<Data>();
 
-		Scanner scanner = new Scanner(new File("document.txt"));
+		Scanner scanner = new Scanner(new File("wordlist.txt"));
 		int position = 0;
 		while (scanner.hasNext()) {
 			String word = scanner.next();
 			Data data = new Data(++position);
 			trie.insert(word, data);
 		}
+
+		long time = System.currentTimeMillis() - start;
+		System.out.println("ms: " + time);
+
 		scanner.close();
 
-		System.out.println(trie);
+		// System.out.println(trie);
+
+		new FileWriter(new File("output.txt")).write(trie.toString());
 	}
 }
