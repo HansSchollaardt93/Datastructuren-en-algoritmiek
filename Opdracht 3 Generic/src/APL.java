55a500b26a6bf202bc53d19d03e93c1643b70605
BenjaminFaal <benjamin_faal@hotmail.com>
2015-01-09 18:40:33 +0100

    fasd

--- Opdracht 3 Generic/src/APL.java
+++ Opdracht 3 Generic/src/APL.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -11,10 +10,7 @@ import java.util.Scanner;
 public class APL {
 
 	public static void main(String[] args) throws IOException {
-		long start = System.currentTimeMillis();
-
 		Trie<Data> trie = new Trie<Data>();
-
 		Scanner scanner = new Scanner(new File("wordlist.txt"));
 		int position = 0;
 		while (scanner.hasNext()) {
@@ -22,14 +18,6 @@ public class APL {
 			Data data = new Data(++position);
 			trie.insert(word, data);
 		}
-
-		long time = System.currentTimeMillis() - start;
-		System.out.println("ms: " + time);
-
 		scanner.close();
-
-		// System.out.println(trie);
-
-		new FileWriter(new File("output.txt")).write(trie.toString());
 	}
 }
